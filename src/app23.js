e81ae23e329cb7f9caaf277083eca071ed3a1367
gopal-sakshi <gopal612@gmail.com>
2022-03-17 12:38:55 +0530

    removed unnecessary stuff

---
+++ src/app23.js
@@ -0,0 +1,5 @@
+function SayingHello() {
+    return <h1>Hello World</h1>;
+}
+
+export default SayingHello;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SayingHello from './app23';
+import Hello from './app24';
+// // Whats the use of <React.StrictMode>
+// ReactDOM.render(
+//   <React.StrictMode> 
+//     <app23 />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// it seems, first letter must be capital... sayingHello didnt work, but SayingHello worked
+ReactDOM.render(<SayingHello />, document.getElementById('root78'));
+ReactDOM.render(<Hello />, document.getElementById('root78'));
